03039bbe6d68118d2434b3a4ef5ccbc0c53d0de8
rosh <oleg.roshnivsky@gmail.com>
2014-05-19 18:06:58 +0300

    PR

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :scm, :git
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{vendor}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
